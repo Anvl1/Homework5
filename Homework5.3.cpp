cc7344fe3aa7e0f15d1630533af15f8bc1c006ba
Anvl1 <88466348+Anvl1@users.noreply.github.com>
2025-06-12 00:27:17 +1000

    Add files via upload

---
+++ Homework5.3.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "rus");
+
+    int mas[3][6] =
+    {
+        {1,2,3,4,5,6},
+        {7,8,9,10,11,12},
+        {13,14,15,16,17,18},
+    };
+
+    std::cout << "Массив:" << std::endl;
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 6; ++j)
+        std::cout << mas[i][j] << " ";
+        std::cout << std::endl;
+    }
+    int min = mas[0][0];
+    int max = mas[0][0];
+    int minIndR = 0, minIndC = 0;
+    int maxIndR = 0, maxIndC = 0;
+
+    for (int i = 0; i < 3; i++) 
+    {
+        for (int j = 0; j < 6; j++) 
+        {
+            if (mas[i][j] < min) 
+            {
+                min = mas[i][j];
+                minIndR = i;
+                minIndC = j;
+            }
+            if (mas[i][j] > max) 
+            {
+                max = mas[i][j];
+                maxIndR = i;
+                maxIndC = j;
+            }
+        }
+    }
+
+    std::cout << "Индекс минимального элемента: " << minIndR << " " << minIndC << std::endl;
+    std::cout << "Индекс максимального элемента: " << maxIndR << " " << maxIndC << std::endl;
+
+    return 0;
+}
---
+++ Homework5.4.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "rus");
+
+    int mas[10] = {10,9,8,7,6,5,4,3,2,1};
+
+    std::cout << "Массив до сортировки: ";
+    for (int i = 0; i < 10; ++i)
+    {
+        std::cout << mas[i] << " ";
+    }
+    std::cout << std::endl;
+
+    for (int j = 0; j < 10 - 1; j++)
+    {
+        for (int i = 9; i > j; i--) 
+        {
+            if (mas[i] < mas[i - 1]) 
+            {
+                int temp = mas[i];
+                mas[i] = mas[i - 1];
+                mas[i - 1] = temp;
+            }
+        }
+    }
+    std::cout << "Массив после сортировки: ";
+    for (int i = 0; i < 10; i++) {
+        std::cout << mas[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
